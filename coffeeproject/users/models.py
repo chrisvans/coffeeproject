a75768497bb2dd45afb009cc556b2cfd1228b94a
chrisvans <chrisvanschyndel@gmail.com>
2013-08-23 19:53:39 -0400

    Moved Extra Model Methods to Core App

--- coffeeproject/users/models.py
+++ coffeeproject/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django_model_shortcuts.models import TimeStampedModel, ExtraModelMethods
+from core.models import TimeStampedModel, ExtraModelMethods
 
 class TestModel(TimeStampedModel, ExtraModelMethods):
     title = models.CharField(max_length=200)
