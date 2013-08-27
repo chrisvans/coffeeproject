fa6b0e9c72f99e3c76d0690712807c0514e440e9
chrisvans <chrisvanschyndel@gmail.com>
2013-08-27 16:23:02 -0700

    Removed Test Models, Fake Tests

--- coffeeproject/users/models.py
+++ coffeeproject/users/models.py
@@ -9,21 +9,3 @@ class CoffeeUser(TimeStampedModel, ExtraModelMethods, AbstractUser):
 
     def get_bills(self):
         return self.bill_set.all()
-
-
-class TestModel(TimeStampedModel, ExtraModelMethods):
-    title = models.CharField(max_length=200)
-
-    def __unicode__(self):
-        return unicode(self.title)
-
-    def get_bills(self):
-        return self.relatedtestmodel_set.all()
-
-class RelatedTestModel(TimeStampedModel, ExtraModelMethods):
-    title = models.CharField(max_length=200)
-    test_model = models.ForeignKey('TestModel')
-
-    def __unicode__(self):
-        return unicode(self.title)
-
--- coffeeproject/users/tests/test_models.py
+++ coffeeproject/users/tests/test_models.py
@@ -1,23 +1,7 @@
 from django.utils import unittest
 from django.test import TestCase
-from users.models import TestModel, RelatedTestModel, CoffeeUser
+from users.models import CoffeeUser
 from billing_data.models import Bill
-
-class UserBillRelationshipTestCase(TestCase):
-
-    def setUp(self):
-        user = TestModel.objects.create(title="user")
-        RelatedTestModel.objects.create(title="bill_1", test_model=user)
-        RelatedTestModel.objects.create(title="bill_2", test_model=user)
-        RelatedTestModel.objects.create(title="bill_3", test_model=user)
-
-    def test_user_has_many_bills(self):
-        user = TestModel.objects.get(title="user")
-        self.assertEqual(user.get_bills().count(), 3)
-
-    def test_bill_has_user(self):
-        bill = RelatedTestModel.objects.get(title="bill_2")
-        self.assertEqual(bill.test_model, TestModel.objects.get(title="user"))
 
 class CoffeeUserBillRelationshipTestCase(TestCase):
 
