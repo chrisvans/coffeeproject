f9f1960b8ac52714f0341c6909cf4f3953858c41
chrisvans <chrisvanschyndel@gmail.com>
2013-09-08 02:30:52 -0400

    Das Herokus III

--- coffeeproject/coffeeproject/settings/base.py
+++ coffeeproject/coffeeproject/settings/base.py
@@ -4,10 +4,6 @@ import os
 from django.core.exceptions import ImproperlyConfigured
 from unipath import Path
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
